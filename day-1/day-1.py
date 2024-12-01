f6ef895f4445e7b217527880806ea6f1ed5f866f
fl4t_is_justice <73598476+fl4tisjustice@users.noreply.github.com>
2024-12-01 14:05:02 +0000

    Part Two

--- day-1/day-1.py
+++ day-1/day-1.py
@@ -1,13 +1,18 @@
 from functools import reduce
+from math import prod
 
-def get_input() -> list[str]:
-    with open("day-1/input.txt", 'r') as file:
-        return file.readlines()
+def get_input() -> tuple[list[int], list[int]]:
+    with open(f"day-1/input.txt", 'r') as file:
+        lines = file.readlines()
+        return reduce(lambda prev, curr : (prev[0] + [curr[0]], prev[1] + [curr[1]]), map(lambda line : tuple(map(int, line.split("   "))), lines), [[],[]])
 
-def solve() -> int:
-    data = get_input()
-    left, right = reduce(lambda prev, curr : (prev[0] + [curr[0]], prev[1] + [curr[1]]), map(lambda line : tuple(map(int, line.split("   "))), data), [[],[]]) 
+def part_one() -> int:
+    left, right = get_input()
     return sum(map(lambda mins : abs(mins[0] - mins[1]), zip(sorted(left), sorted(right))))
 
+def part_two() -> int:
+    left, right = get_input()
+    return sum(map(prod, zip(left, [right.count(value) for value in left])))
+
 if __name__ == "__main__":
-    print(solve())
+    print(f"Part One: { part_one() }\nPart Two: { part_two() }")
