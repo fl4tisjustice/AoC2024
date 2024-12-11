26cdaa7e36f04dadba61dec481b3627e6feb47b0
fl4tisjustice <chaoticflat@gmail.com>
2024-12-11 14:59:38 +0000

    Spacing

--- day-11/day11.hs
+++ day-11/day11.hs
@@ -1,17 +1,6 @@
 import GHC.Utils.Misc (split)
 import qualified Data.IntMultiSet as IMS (fromList, concatMap, size)
 
-getInput :: IO [Int]
-getInput =  map read . split ' ' <$> readFile "day-11/input.txt" :: IO [Int]
-
-blink :: Int -> [Int]
-blink stone
-    | stone == 0 = [1]
-    | even . length $ stoneStr = map read [take half stoneStr, drop half stoneStr]
-    | otherwise = [2024 * stone]
-    where stoneStr = show stone
-          half = length stoneStr `div` 2
-
 -- Part 2 based on https://work.njae.me.uk/2024/12/11/advent-of-code-2024-day-11/
 -- (Incidentally my part 1 implementation was virtually identical)
 -- Was not aware of the "infamous" Lanternfish example from a previous year
@@ -30,6 +19,16 @@ blink stone
 -- A further possible optimization could be caching previous computations,
 -- i.e. DP memoisation due to overlapping subproblems
 
+getInput :: IO [Int]
+getInput =  map read . split ' ' <$> readFile "day-11/input.txt" :: IO [Int]
+
+blink :: Int -> [Int]
+blink stone
+    | stone == 0 = [1]
+    | even . length $ stoneStr = map read [take half stoneStr, drop half stoneStr]
+    | otherwise = [2024 * stone]
+    where stoneStr = show stone
+          half = length stoneStr `div` 2
 
 blinkN :: Int -> [Int] -> Int
 blinkN times stones = IMS.size $ (!! times) $ IMS.concatMap blink `iterate` IMS.fromList stones
