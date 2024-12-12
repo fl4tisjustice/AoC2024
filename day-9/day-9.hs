8935e70d85d3e4c031c330ec84151ff10fbc19b3
fl4tisjustice <chaoticflat@gmail.com>
2024-12-12 01:35:21 +0000

    Removed redundant code in Day 9 Part 1

--- day-9/day-9.hs
+++ day-9/day-9.hs
@@ -1,7 +1,6 @@
 import Data.Char (digitToInt)
 import Text.Printf (printf)
 
-type DiskMap = [Block]
 type CompressedDiskMap = [(Int, Block)]
 
 data Block = Empty | File Int
@@ -12,22 +11,15 @@ instance Show Block where
         File id -> show id
         Empty -> "."
 
-diskMap :: String -> DiskMap
-diskMap input = fileBlock 0 $ map digitToInt input
-    where
-        fileBlock id (size:rest) = size `replicate` File id ++ emptyBlock (id + 1) rest
-        fileBlock _ [] = []
-        emptyBlock id (size:rest) =  size `replicate` Empty ++ fileBlock id rest
-        emptyBlock _ [] = []
-
-compress :: DiskMap -> CompressedDiskMap
-compress [] = []
-compress blocks@(block:_) = (length identical, block) : compress rest
-    where (identical, rest) = span (==block) blocks
-
+compressedDiskMap :: String -> CompressedDiskMap
+compressedDiskMap input = fileBlock input 0
+    where fileBlock [] _ = []
+          fileBlock (count:rest) idx = (digitToInt count, File idx) : emptyBlock rest (idx + 1)
+          emptyBlock [] _ = []
+          emptyBlock (count:rest) idx = (digitToInt count, Empty) : fileBlock rest idx
 
 blockFragmenter :: CompressedDiskMap -> CompressedDiskMap
-blockFragmenter compressed = go compressed $ reverse . filter (\x -> snd x /= Empty) $ compressed
+blockFragmenter compressed = go compressed $ reverse . filter ((/=Empty) . uncurry seq) $ compressed
     where go _ [last] = [last]
           go (file@(_, File _) : rest) toInsert = file : rest `go` init toInsert
           go ((emptyCount, _) : rest) toInsert =
@@ -41,10 +33,10 @@ checksum :: CompressedDiskMap -> Int
 checksum fragmented = go fragmented 0
     where go [] _ = 0 
           go ((count, File id) : rest) idx =
-            sum (map (* id) [idx..idx + count - 1]) + go rest (idx + count)
+            (sum . map (* id)) [idx..idx + count - 1] + go rest (idx + count)
 
 main :: IO ()
 main = do
     contents <- readFile "input.txt"
-    let compressed = compress . diskMap $ contents
+    let compressed = compressedDiskMap contents
         in printf "Part One: %d\n" . checksum . blockFragmenter $ compressed
