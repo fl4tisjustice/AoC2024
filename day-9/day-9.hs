24f3d3d71ea6b9f673b9db2b9b60bbd9c1c1d3c2
fl4tisjustice <chaoticflat@gmail.com>
2024-12-13 18:33:34 +0000

    Day 9 - Part 2
    FINALLYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY AND IT'S FAST

--- day-9/day-9.hs
+++ day-9/day-9.hs
@@ -1,5 +1,8 @@
-import Data.Char (digitToInt)
 import Text.Printf (printf)
+import Data.Char (digitToInt)
+import Data.Foldable (Foldable(toList))
+import Data.Sequence (Seq, fromList, Seq((:<|), (:|>)), (><), (|>), (<|), spanl, singleton, viewl, breakl)
+import qualified Data.Sequence as S (null, Seq(Empty), drop)
 
 type CompressedDiskMap = [(Int, Block)]
 
@@ -16,10 +19,11 @@ compressedDiskMap input = fileBlock input 0
     where fileBlock [] _ = []
           fileBlock (count:rest) idx = (digitToInt count, File idx) : emptyBlock rest (idx + 1)
           emptyBlock [] _ = []
+          emptyBlock ('0':rest) idx = fileBlock rest idx
           emptyBlock (count:rest) idx = (digitToInt count, Empty) : fileBlock rest idx
 
 blockFragmenter :: CompressedDiskMap -> CompressedDiskMap
-blockFragmenter compressed = go compressed $ reverse . filter ((/=Empty) . uncurry seq) $ compressed
+blockFragmenter compressed = go compressed $ reverse . filter ((/=Empty) . snd) $ compressed
     where go _ [last] = [last]
           go (file@(_, File _) : rest) toInsert = file : rest `go` init toInsert
           go ((emptyCount, _) : rest) toInsert =
@@ -29,14 +33,26 @@ blockFragmenter compressed = go compressed $ reverse . filter ((/=Empty) . uncur
                 EQ -> rest `go` tail toInsert
                 GT -> ((emptyCount - filesize, Empty) : rest) `go` tail toInsert
 
+fileFragmenter :: CompressedDiskMap -> CompressedDiskMap
+fileFragmenter = toList . go . fromList
+    where go S.Empty = S.Empty
+          go (occupied@(_, File _) :<| rest) = occupied <| go rest
+          go (rest :|> empty@(_, Empty)) = go rest |> empty 
+          go (search :|> file@(filesize, File _)) = 
+            let (left, candidate) = spanl (\(size, block) -> size < filesize || block /= Empty) search
+            in case candidate of
+                S.Empty -> go search |> file
+                ((size, Empty) :<| _) -> go (left >< singleton file >< leftover >< S.drop 1 candidate) |> (filesize, Empty)
+                    where leftover = if filesize == size then S.Empty else singleton (size - filesize, Empty)
+
 checksum :: CompressedDiskMap -> Int
 checksum fragmented = go fragmented 0
-    where go [] _ = 0 
+    where go [] _ = 0
+          go ((count, Empty) : rest) idx = go rest (idx + count)
           go ((count, File id) : rest) idx =
             (sum . map (* id)) [idx..idx + count - 1] + go rest (idx + count)
 
 main :: IO ()
 main = do
-    contents <- readFile "input.txt"
-    let compressed = compressedDiskMap contents
-        in printf "Part One: %d\n" . checksum . blockFragmenter $ compressed
+    compressed <- pure <$> compressedDiskMap =<< readFile "input.txt"
+    printf "Part One: %d\nPart Two: %d\n" (checksum . blockFragmenter $ compressed) (checksum . fileFragmenter $ compressed)
