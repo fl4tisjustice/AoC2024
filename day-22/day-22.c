d7b4f4e2045d8e1f77433364b15b94661e14a82d
fl4tisjustice <chaoticflat@gmail.com>
2024-12-24 06:21:50 +0000

    Less magic numbers

--- day-22/day-22.c
+++ day-22/day-22.c
@@ -2,7 +2,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define GENERATIONS 2000
+
 #define PRUNE(n) ((1 << n) - 1)
+#define MAX_SECRET_LENGTH 24
 
 size_t get_input(uint32_t **initial) {
     FILE *file = fopen("day-22/input.txt", "r");
@@ -17,9 +20,9 @@ size_t get_input(uint32_t **initial) {
     *initial = (uint32_t*)calloc(secrets, sizeof(uint32_t));
 
     // All secrets are calculated mod 24, so they'll never exceed this number
-    char buffer[28 + 1] = { 0 };
+    char buffer[MAX_SECRET_LENGTH + 1] = { 0 };
     for (size_t idx = 0; idx < secrets; idx++) {
-        fgets(buffer, 28 + 1, file);
+        fgets(buffer, MAX_SECRET_LENGTH + 1, file);
         (*initial)[idx] = (uint32_t)strtoul(buffer, NULL, 10);
     }
 
@@ -27,13 +30,13 @@ size_t get_input(uint32_t **initial) {
     return secrets;
 }
 
-uint64_t simulate(uint32_t *initial, size_t secrets, size_t generations) {
+uint64_t simulate(uint32_t *initial, size_t secrets) {
     for (size_t idx = 0; idx < secrets; idx++) {
         uint32_t seed = initial[idx];
-        for (size_t counter = 0; counter < generations; counter++) {
-            seed = ((seed << 6) ^ seed) & PRUNE(24);
-            seed = ((seed >> 5) ^ seed) & PRUNE(24);
-            seed = ((seed << 11) ^ seed) & PRUNE(24);
+        for (size_t counter = 0; counter < GENERATIONS; counter++) {
+            seed = ((seed << 6) ^ seed) & PRUNE(MAX_SECRET_LENGTH);
+            seed = ((seed >> 5) ^ seed) & PRUNE(MAX_SECRET_LENGTH);
+            seed = ((seed << 11) ^ seed) & PRUNE(MAX_SECRET_LENGTH);
         }
         initial[idx] = seed;
     }
@@ -46,7 +49,7 @@ uint64_t simulate(uint32_t *initial, size_t secrets, size_t generations) {
 int main() {
     uint32_t *initial = NULL;
     size_t secrets = get_input(&initial);
-    printf("Part One: %lu\n", simulate(initial, secrets, 2000));
+    printf("Part One: %lu\n", simulate(initial, secrets));
 
     free(initial);
     return 0;
